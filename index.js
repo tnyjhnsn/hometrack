1a848f23ef5e4da1f59ecf1a4b3f33535c4a84d9
Tony Johnson <tony@tosp.net.au>
2018-08-23 17:24:12 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
+
+app.use((err, req, res, next) => {
+  if (err.status === 400) {
+    return res.status(err.status).send({
+      error: 'Could not decode request: JSON parsing failed'
+    })
+  }
+  return next(err)
+})
+
+app.post('/', (req, res) => {
+
+  const properties = req.body.payload.filter(property => {
+    return property.type === 'htv' && property.workflow === 'completed'
+  })
+
+  const report = properties.map(property => {
+    const {
+      unitNumber,
+      buildingNumber,
+      street,
+      suburb,
+      state,
+      postcode
+    } = property.address
+
+    const concatnumber = `${unitNumber || ''} ${buildingNumber}`.trim()
+    const concataddress = `${concatnumber} ${street} ${suburb} ${state} ${postcode}`
+
+    return {
+      concataddress,
+      type: property.type,
+      workflow: property.workflow
+    }
+  })
+  
+  res.status(200).send({
+    response: report
+  })
+})
+
+let port = 5000
+
+app.listen(port, () => {
+  console.log(`Hometrack listening on port ${port} ...`)
+})
