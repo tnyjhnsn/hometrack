778c8e5ef70ccbafe14fed8e53f85e60da9fc1bb
Tony Johnson <tony@tosp.net.au>
2018-08-23 17:36:40 +1000

    Modify PORT for heroku

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ app.post('/', (req, res) => {
   })
 })
 
-let port = 5000
+const PORT = process.env.PORT || 5000
 
-app.listen(port, () => {
-  console.log(`Hometrack listening on port ${port} ...`)
+app.listen(PORT, () => {
+  console.log(`Hometrack listening on port ${PORT} ...`)
 })
